e3c471db88013a8f8fd6ca0c2424e1175eb92f87
cactus-jacck <aayushnag@gmail.com>
2025-04-02 22:36:29 +0530

    changed the base url to railway app

--- frontend/src/component/config/Api.js
+++ frontend/src/component/config/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-export const API_URL = "http://localhost:8080"
-// export const API_URL = "https://food-order-app-production-44a4.up.railway.app"
+// export const API_URL = "http://localhost:8080"
+export const API_URL = "https://food-order-app-production-44a4.up.railway.app"
 
 export const api = axios.create({
     baseURL:API_URL,
