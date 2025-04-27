6af2ec6c41b257cc7e5468fb0d4153ceedc875f0
cactus-jacck <aayushnag@gmail.com>
2025-04-27 18:31:12 +0530

    changed backend service

--- frontend/src/component/config/Api.js
+++ frontend/src/component/config/Api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 // export const API_URL = "http://localhost:8080"
-export const API_URL = "https://food-order-app-production-44a4.up.railway.app"
+// export const API_URL = "https://food-order-app-production-44a4.up.railway.app"
+export const API_URL = "https://eatzyv2.onrender.com"
 
 export const api = axios.create({
     baseURL:API_URL,
